c7e7a6c93d8a07a8f6a5e614ab61a0e1ab6aad00
Yeji-Yoon <yoonyeji12@naver.com>
2024-01-15 17:56:10 +0900

    게시판 추가 기능

--- src/main/java/org/choongang/admin/board/controllers/BoardSearch.java
+++ src/main/java/org/choongang/admin/board/controllers/BoardSearch.java
@@ -5,7 +5,7 @@ import lombok.Data;
 @Data
 public class BoardSearch {
     private int page = 1;
-    private int limit = 20;
+    private int limit = 20;rptl
 
     private String bid;
     private String bName;
